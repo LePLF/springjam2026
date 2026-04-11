52f044da656fd2275b4189eea7b3666e6ab70825
mare.charnay <marec.charnay@brassart.fr>
2026-04-11 15:47:52 +0200

    ui

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+    
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
+
