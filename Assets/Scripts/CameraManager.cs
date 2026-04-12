e745c591f48391bee18890430e028d5c0f2f15a8
mare.charnay <marec.charnay@brassart.fr>
2026-04-12 10:01:46 +0200

    Shakira

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { get; private set; }
+
+    [Header(" CinemachineCamera type ")]
+    [SerializeField] private CinemachineImpulseSource impulseSource;
+
+    [Header("Shake léger")]
+    [SerializeField] private float lightAmplitude = 0.5f;
+    [SerializeField] private float lightDuration   = 0.15f;
+
+    [Header("Shake Hard")]
+    [SerializeField] private float heavyAmplitude = 2f;
+    [SerializeField] private float heavyDuration   = 0.35f;
+
+    private void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+    }
+
+    public void ShakeLight()              => Shake(lightAmplitude, lightDuration);
+    public void ShakeHeavy()              => Shake(heavyAmplitude, heavyDuration);
+    public void ShakeCustom(float a, float d) => Shake(a, d);
+
+    private void Shake(float amplitude, float duration)
+    {
+        if (impulseSource == null) return;
+        impulseSource.ImpulseDefinition.ImpulseDuration = duration;
+        impulseSource.GenerateImpulse(amplitude);
+    }
+}
