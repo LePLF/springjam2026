7fc358ba1ac8cc42905036cd897716ec9ba92956
mare.charnay <marec.charnay@brassart.fr>
2026-04-12 11:47:47 +0200

    MoovCameEndokkkkkkkkkkkkk

---
+++ Assets/Scripts/UI/MooveCameraEnd.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MooveCameraEnd : MonoBehaviour
+{
+    [SerializeField] private Vector2   pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float     moveSpeed = 5f;
+
+    private Vector2 _target;
+
+    private void Start()
+    {
+        _target = pointA;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            _target = _target == pointA ? (Vector2)pointB.position : pointA;
+
+        transform.position = Vector2.MoveTowards(transform.position, _target, moveSpeed * Time.deltaTime);
+    }
+}
